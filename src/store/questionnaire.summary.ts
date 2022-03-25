b189b4fb22c5ac0a6d86d7131a4868ecb49688ba
nodematch <genn1997@yahoo.co.jp>
2022-03-25 15:52:36 +0900

    update singledataba2

--- src/store/questionnaire.summary.ts
+++ src/store/questionnaire.summary.ts
@@ -1,12 +1,13 @@
 import { singleDataOmi } from "./questionnaire.omi.single";
+import { singleDataBa2 } from "./questionnaire.ba2.single";
 import { multiData } from "./questionnaire.multi";
 import { resultDataOmi } from "./questionnaire.omi.result";
 import { resultDataBa2 } from "./questionnaire.ba2.result";
 export const questionSet = {
   "omicron": { single: singleDataOmi.qst, multi: multiData.qst, result: resultDataOmi.list },
-  "ba2": { single: singleDataOmi.qst, multi: multiData.qst, result: resultDataBa2.list },
+  "ba2": { single: singleDataBa2.qst, multi: multiData.qst, result: resultDataBa2.list },
 };
 export const answerSet = {
   "omicron": { single: singleDataOmi.ans, multi: multiData.ans },
-  "ba2": { single: singleDataOmi.ans, multi: multiData.ans },
+  "ba2": { single: singleDataBa2.ans, multi: multiData.ans },
 }
